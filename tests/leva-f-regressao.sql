d90d7ae3ded95ca86ebc7f432b503f26f2182020
harmonizequip-cmyk <harmonizequip@gmail.com>
2026-09-25 05:42:13 -0300

    Update leva-f-regressao.sql

--- tests/leva-f-regressao.sql
+++ tests/leva-f-regressao.sql
@@ -327,5 +327,6 @@ end $$;
 
 rollback;
 
+-- rollback de propósito: este script só verifica comportamento, não
 -- deve deixar dado de teste no banco (nem em ambiente local nem, por
 -- engano, se alguém apontar isso para outro banco).
